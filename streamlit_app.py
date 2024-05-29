0382211ebb01246b7dc7f9f2747dc0bc11cd3450
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-05-29 14:26:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -259,7 +259,7 @@ if not residuals_df.empty:
         ax.set_title(f"{name}", fontsize=8)
         ax.set_xlabel('ReadTime', fontsize=8)
         ax.set_ylabel('Values', fontsize=8)
-        ax.legend(fontsize=6)
+        ax.legend(fontsize=5)
         ax.tick_params(axis='x', labelsize=6)
         ax.tick_params(axis='y', labelsize=6)
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
